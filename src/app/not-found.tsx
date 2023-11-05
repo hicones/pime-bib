93b7c843756048274af796f548c96dc763148566
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2023-11-04 21:02:19 -0300

    feat: add notfound page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-background-700">
+      <h1 className="text-9xl text-white font-bold text-center">404</h1>
+      <h2 className="text-4xl text-fontcolor-50 font-bold text-center">
+        Esta página é como um tesouro perdido. <br /> Infelizmente, não a
+        encontramos.
+      </h2>
+
+      <h3 className="absolute text-2xl bottom-10 text-white font-bold">
+        @paulistinho
+      </h3>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import { ContactUs } from "@/components/Home/ContactUs";
 import { FirstSection } from "@/components/Home/FirstSection";
+import { Footer } from "@/components/Home/Footer";
 import { SecondSection } from "@/components/Home/SecondSection";
 import { ThirdSection } from "@/components/Home/ThirdSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <FirstSection />
-      <SecondSection />
-      <ThirdSection />
-      <ContactUs />
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center">
+        <FirstSection />
+        <SecondSection />
+        <ThirdSection />
+        <ContactUs />
+      </main>
+      <Footer />
+    </>
   );
 }
