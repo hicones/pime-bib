ad3390148e65997d5dd588ba1662387a9afba696
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2023-12-03 20:04:37 -0300

    feat: update gif

--- src/components/Home/HomeBackground.tsx
+++ src/components/Home/HomeBackground.tsx
@@ -1,11 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
 export const HomeBackground = () => {
   return (
     <section className="homeBackground -z-10 flex items-center justify-center">
       <div>
-        <iframe
-          src="https://giphy.com/embed/Riu86b28LvzXeXkzF1"
-          allowFullScreen
-        ></iframe>
+        <img
+          src="/gif-bib.gif"
+          alt="gif"
+          className="lg:max-w-xl w-full md:max-w-lg max-w-xs"
+        />
       </div>
       <span className="font-normal grotesk absolute -rotate-90 top-1/2 left-0 lg:text-[2rem] md:text-2xl text-base uppercase">
         seu guia
