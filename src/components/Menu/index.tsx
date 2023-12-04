2f45374add1bc6086561cb0ba44968026fda3763
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2023-12-03 21:03:12 -0300

    feat: update

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -35,6 +35,7 @@ export const Menu = () => {
             <Link
               href="/"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               01
               <span className="text-fontcolor-50">início</span>
@@ -42,6 +43,7 @@ export const Menu = () => {
             <Link
               href="/destaques"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               02
               <span className="text-fontcolor-50">destaques</span>
@@ -49,6 +51,7 @@ export const Menu = () => {
             <Link
               href="/bares"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               03
               <span className="text-fontcolor-50">bares</span>
@@ -56,6 +59,7 @@ export const Menu = () => {
             <Link
               href="/restaurantes"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               04
               <span className="text-fontcolor-50">restaurantes</span>
@@ -65,13 +69,15 @@ export const Menu = () => {
             <Link
               href="/cultura"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               05
               <span className="text-fontcolor-50">cultura</span>
             </Link>
             <Link
-              href="/hitorico"
+              href="/historico"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               06
               <span className="text-fontcolor-50">historico</span>
@@ -79,6 +85,7 @@ export const Menu = () => {
             <Link
               href="/fale-conosco"
               className={`flex justify-start items-start gap-2 checkmark`}
+              onClick={() => setOpen(() => false)}
             >
               07
               <span className="text-fontcolor-50">fale conosco</span>
@@ -92,7 +99,7 @@ export const Menu = () => {
           className="absolute top-12 right-10 cursor-pointer hover:opacity-80 transition duration-300 w-[52px] h-[52px] hover:-rotate-90"
           onClick={() => setOpen(() => false)}
         >
-          <g clip-path="url(#clip0_978_1861)">
+          <g clipPath="url(#clip0_978_1861)">
             <path
               d="M32 0C14.327 0 0 14.327 0 32C0 49.674 14.327 64 32 64C49.674 64 64 49.674 64 32C64 14.327 49.674 0 32 0ZM32 60.063C16.561 60.063 4 47.439 4 31.9999C4 16.5609 16.561 3.99988 32 3.99988C47.439 3.99988 60 16.5609 60 31.9999C60 47.4388 47.439 60.063 32 60.063ZM43.313 20.687C42.532 19.906 41.266 19.906 40.485 20.687L32 29.172L23.515 20.687C22.734 19.906 21.468 19.906 20.686 20.687C19.905 21.468 19.905 22.734 20.686 23.515L29.171 32L20.686 40.485C19.905 41.265 19.905 42.5331 20.686 43.3131C21.467 44.0941 22.733 44.0941 23.515 43.3131L32 34.8281L40.485 43.3131C41.266 44.0941 42.532 44.0941 43.313 43.3131C44.094 42.5331 44.094 41.265 43.313 40.485L34.828 32L43.313 23.515C44.095 22.733 44.095 21.467 43.313 20.687Z"
               fill="#FFFEFB"
