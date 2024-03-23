31c742008d21ef3ddce3d3672ba1adbcd9e7e9e5
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2024-03-23 00:24:42 -0300

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+
+export default function Error() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-main-50 gap-10">
+      <h1 className="text-9xl text-white font-normal text-center montserrat">
+        4O4
+      </h1>
+      <h2 className="lg:text-4xl text-xl text-fontcolor-50 font-normal text-center grotesk uppercase px-2">
+        Ocorreu um erro em nossos servidores. <br /> Por favor, tente novamente
+        mais tarde.
+      </h2>
+      <div className="flex absolute bottom-16">
+        <Link
+          href="/"
+          className="hoverText1 w-fit lg:text-2xl text-base font-normal uppercase"
+        >
+          <span className="hoverText2">voltar para página inicial</span>
+          <span className="hoverText2">voltar para página inicial</span>
+        </Link>
+      </div>
+    </main>
+  );
+}
